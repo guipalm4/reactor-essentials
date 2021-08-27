c9c41a8c917731e969a23a1323eac5fa953ddd09
Guilherme Palma <guilherme_gomes@terceiros.sicredi.com.br>
2021-08-27 17:41:16 -0300

    03 - Subscribing to Mono

--- src/test/java/guipalm4/reactor/test/MonoTest.java
+++ src/test/java/guipalm4/reactor/test/MonoTest.java
@@ -2,12 +2,27 @@ package guipalm4.reactor.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
 public class MonoTest {
 
     @Test
-    void test() {
-        log.info("Everything working as intended");
+    void monoSubscriber() {
+        //Given
+        log.info("-------Actual------------");
+        var name = "guipalm4";
+        Mono<String> nameMono = Mono.just(name)
+                .log();
+        //When
+        nameMono.subscribe();
+
+        //Then
+        log.info("-------Expected------------");
+        StepVerifier.create(nameMono)
+                .expectNext(name)
+                .verifyComplete();
+
     }
 }
