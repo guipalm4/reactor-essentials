bb55e25233b6af25724ce855858ad29802669c94
Guilherme Palma <guilherme_gomes@terceiros.sicredi.com.br>
2021-08-27 22:00:48 -0300

    04 - Subscribing to Mono with Consumer and Error

--- src/test/java/guipalm4/reactor/test/MonoTest.java
+++ src/test/java/guipalm4/reactor/test/MonoTest.java
@@ -12,17 +12,56 @@ public class MonoTest {
     void monoSubscriber() {
         //Given
         log.info("-------Actual------------");
+
         var name = "guipalm4";
-        Mono<String> nameMono = Mono.just(name)
-                .log();
+        Mono<String> nameMono = Mono.just(name).log();
+
         //When
         nameMono.subscribe();
 
         //Then
         log.info("-------Expected------------");
+
         StepVerifier.create(nameMono)
                 .expectNext(name)
                 .verifyComplete();
+    }
 
+    @Test
+    void monoSubscriberConsumer() {
+        //Given
+        log.info("-------Actual------------");
+
+        var name = "guipalm4";
+        Mono<String> nameMono = Mono.just(name).log();
+
+        //When
+        nameMono.subscribe(s-> log.info("The dev is {}", s));
+
+        //Then
+        log.info("-------Expected------------");
+
+        StepVerifier.create(nameMono)
+                .expectNext(name)
+                .verifyComplete();
+    }
+
+    @Test
+    void monoSubscriberConsumerError() {
+        //Given
+        log.info("-------Actual------------");
+
+        var name = "guipalm4";
+        Mono<String> nameMono = Mono.just(name)
+                .map(s-> {throw new RuntimeException("Testing mono with error");});
+
+        //When
+        nameMono.subscribe(s-> log.info("User {}", s), s-> log.error("Something bad happened"));
+        nameMono.subscribe(s-> log.info("User {}", s), Throwable::printStackTrace);
+
+        //Then
+        StepVerifier.create(nameMono)
+                .expectError(RuntimeException.class)
+                .verify();
     }
 }
